5eb3f99c1698cdb0a13fc1934c08d3775a712084
Oleksandr Obukhivskyi <alexobuhovskiy@gmail.com>
2020-04-08 12:52:19 +0300

    initial commit

---
+++ src/TestWeb/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet("probe")]
+        public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+    }
+}
